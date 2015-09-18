748c781768f6da6802df889f41dd589f8543c6a8
Nitish Victor <nithishvictor@gmail.com>
2015-09-19 00:10:13 +0530

    Sign up and login

---
+++ ptm-app/www/js/config.js
@@ -0,0 +1,5 @@
+angular.module('starter.config', ['ionic', 'starter.controllers', 'starter.services'])
+.constant('urlConfig', {
+  'backend': 'http://localhost:1337/',
+  'version': 0.1
+})
--- ptm-app/www/js/controllers.js
+++ ptm-app/www/js/controllers.js
@@ -1,6 +1,6 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ['ionic', 'starter.config'])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', function($scope, $ionicModal, $ionicPopup, $timeout,$state, $http, urlConfig) {
 
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
@@ -10,8 +10,13 @@ angular.module('starter.controllers', [])
   //});
 
   // Form data for the login modal
-  $scope.loginData = {};
-
+  $scope.loginData = {
+  };
+  //Form data for the sign up page
+  $scope.signUpData = {
+    choice:'Parent'
+  };
+  $scope.message = '';
   // Create the login modal that we will use later
   $ionicModal.fromTemplateUrl('templates/login.html', {
     scope: $scope
@@ -29,10 +34,28 @@ angular.module('starter.controllers', [])
     $scope.modal.show();
   };
 
+  // An alert dialog
+ $scope.showAlert = function(message) {
+   var alertPopup = $ionicPopup.alert({
+     title: 'Message',
+     template: message
+   });
+   alertPopup.then(function(res) {
+     console.log('Sign up alert shown');
+   });
+ }
+
   // Perform the login action when the user submits the login form
   $scope.doLogin = function() {
     console.log('Doing login', $scope.loginData);
-
+    var loginUrl = urlConfig.backend+"auth";
+    $http.post(loginUrl,$scope.loginData)
+    .error(function(data, status, headers, config) {
+      $scope.showAlert('Invalid Username/Password');
+    })
+    .then(function(res){
+      $state.transitionTo('app.browse');
+    });
     // Simulate a login delay. Remove this and replace with your login
     // code if using a login system
     $timeout(function() {
@@ -40,9 +63,47 @@ angular.module('starter.controllers', [])
     }, 1000);
   };
 
+  $scope.radioChange = function () {
+    console.log($scope.signUpData.choice);
+    var choice = $scope.signUpData.choice;
+    if(choice=='Parent'){
+      document.getElementById("enrollmentNumber").value = " Not Required";
+      document.getElementById("enrollmentNumber").disabled = true;
+    }
+    else {
+        document.getElementById("enrollmentNumber").value = "";
+        document.getElementById("enrollmentNumber").disabled = false;
+    }
+  }
   //Logic for sign up
-  $scope.doSignUp = function(){
-    
+  $scope.doSignUp = function(form){
+    if(form.$valid)
+    {
+      var data = $scope.signUpData;
+      var createUrl = '';
+      if($scope.signUpData.choice=='Parent')
+      {
+        createUrl =  urlConfig.backend+'parents/create';
+        if(data.registrationNo){
+          delete data.registrationNo;
+        }
+      }
+      else {
+        createUrl =  urlConfig.backend+'teacher/create';
+      }
+      delete data.choice;
+      $http.post(createUrl,data).then(function(res) {
+        console.log(res);
+        if(res.statusText=='Created'){
+          $scope.showAlert("Sign up succesful!");
+          $state.go('app.login');
+        }
+        else{
+          $scope.showAlert("Oops, Error in signing you up!")
+        }
+      });
+
+    }
   };
 })
 
@@ -54,7 +115,7 @@ angular.module('starter.controllers', [])
   //
   //$scope.$on('$ionicView.enter', function(e) {
   //});
-  
+
   $scope.chats = Chats.all();
   $scope.remove = function(chat) {
     Chats.remove(chat);
