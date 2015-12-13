67605c22fc5cfecc5696254c9424a1d020dd0a3e
Abhimanyu <abhimanyu_arya@external.mckinsey.com>
2015-12-14 00:56:37 +0530

    removed tmp files

--- ptm-app/www/js/config.js
+++
@@ -1,5 +0,0 @@
-angular.module('starter.config', ['ionic', 'starter.controllers', 'starter.services'])
-.constant('urlConfig', {
-  'backend': 'https://rotvicdspygeek.herokuapp.com/',//'http://192.168.1.107:1337/',
-  'version': 0.1
-});
