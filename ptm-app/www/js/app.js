716e62d3277ef402502d958b2a9e4aa6be2b6e19
Abhimanyu <abhimanyu_arya@external.mckinsey.com>
2015-12-14 09:18:21 +0530

    fixed UI flickering

--- ptm-app/www/js/app.js
+++ ptm-app/www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic','ionic.service.core','ionic.service.analytics', 'starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic','ionic.service.core','ionic.service.analytics', 'starter.controllers', 'starter.services','ngIOS9UIWebViewPatch'])
 
 .run(function($ionicPlatform, $ionicAnalytics, $cordovaPush, sessionService, $state) {
   $ionicPlatform.ready(function() {
